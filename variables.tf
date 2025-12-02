594f796cbd98dcff36391e48a5b7d6d460f97f5b
nadajarboui99 <nada.jarboui@insat.ucar.tn>
2025-12-02 15:23:49 +0100

    change pass

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "db_user" {
 variable "db_password" {
   description = "Mot de passe PostgreSQL (ATTENTION: Simulé, ne pas utiliser en Prod !)"
   type        = string
-  default     = "strongpassword123"
+  default     = "strongpassword12"
 }
 
 # --- Variables d'Application ---
